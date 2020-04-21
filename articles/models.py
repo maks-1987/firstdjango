ed8540674b208b85afec566ff6f346866074bc28
MaxD <max.moderation@gmail.com>
2020-04-21 21:51:03 +0300

    start django 59

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,7 @@
+import datetime
 from django.db import models
+
+from django.utils import timezone
 
 
 # Объясняем Django из чего состоит приложение, что хранить в БД
@@ -7,8 +10,19 @@ class Article(models.Model):  # This is first model (называть в ед ч
     article_text = models.TextField('текст статьи')
     pub_date = models.DateField('дата публикации')
 
+    def __str__(self):
+        return self.article_title
+
+    # покажет отображалась ли статья недавно (напр 7 дней назад)
+    def was_published_recently(self):
+        return self.pub_date >= (timezone.now() - datetime.timedelta(days=7))
+
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE) # все комментар на сайте будут привязаны к статьям Article
+    article = models.ForeignKey(Article,
+                                on_delete=models.CASCADE)  # все комментар на сайте будут привязаны к статьям Article
     author_name = models.CharField('имя автора', max_length=45)
     comment_text = models.CharField('текст комментария', max_length=200)
+
+    def __str__(self):
+        return self.author_name
